26706933c9b5edc116bffc72fbb5c11d40dd88ad
Santiago Figueredo <figueredosantiago2000@gmail.com>
2025-08-27 02:50:24 -0300

    SQL Notes

--- SQL/SQLQuery3.sql
+++ SQL/SQLQuery3.sql
@@ -1,11 +1,37 @@
--- Query SQL
+-- SQL Query
 
--- LEER
+-- Hay cuatro consultas basicas: 
 
-select --para leer-- * --para seleccionar todo from
+-- LEER --
+
+-- "select" para leer  "*" para seleccionar todo "from" (tabla)
 
 select * from TIPOSEDICION
+select * from DISCOS
+select Titulo, CantidadCanciones, Descripcion from DISCOS, ESTILOS --Devuelve columas especificas de una tabla.
+select * from ESTILOS
+-- "where" es un condicional:
+-- Podemos usar concatenadores logicos: AND y OR
 
---insertar 
---modificar  
---eliminar
+select * from DISCOS where idestilo = 2 AND CantidadCanciones = 12
+
+select D.Titulo, D.CantidadCanciones, E.Descripcion as Estilo from DISCOS D, ESTILOS E -- "E" y "D" son seudonimos
+where D.IdEstilo = E.Id 
+
+-- INSERTAR --
+
+-- "insert" para escribir en la base de datos
+
+insert into DISCOS (Titulo, CantidadCanciones) values ('Powerslave', 10) --para cargar nuevo/s elemento/s
+--otra forma:
+insert into DISCOS values
+('Killers', GETDATE(), 9, 'https...', 2, 3 ) --cargamos los elementos de todas las columnas.
+
+-- MODIFICAR --
+
+update ESTILOS set Descripcion = 'Country' /* MUY IMPORTANTE USAR "WHERE" */ where id = 1
+select D.Titulo, D.Titulo, E.Descripcion as estilo from DISCOS D, ESTILOS E
+
+-- ELIMINAR --
+
+delete from DISCOS WHERE id = 5
